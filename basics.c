237059213f6203127ddd205e468763292f286369
loganthebeing <logansd01@gmail.com>
2025-01-08 13:27:43 -0500

    Create basics.c

---
+++ basics.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Functions are defined at the top of a program */
+int mean(int a, int b)
+{
+    /* Calculates the mean of the int arguments a and b */
+    return (a + b)/2;
+}
+
+/* Execution starts here at the main function */
+int main()
+{
+    /* ';' ends a statement */
+    int i, j;
+    int answer;
+
+    i = 7;
+    j = 9;
+
+    /* Stores the value of the mean of i and j in the int variable answer */
+    answer = mean(i, j);
+
+    /* Prints answer using printf. %d is a placeholder for int values. Use %f for floating-point numbers and %s for strings. */
+    printf("The mean of %d and %d is %d\n", i, j, answer);
+
+    /* Program ends here */
+    return 0;
+}
+
+/* Compile the program using: gcc -o basics basics.c
+   - 'gcc' is the GNU C compiler.
+   - '-o basics' specifies the name of the output executable file (basics). */
